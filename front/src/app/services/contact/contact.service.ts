b251f30adc3207dbcf9a5aab955a471a48ecb5e3
GeekGuste <manouauguste7@gmail.com>
2026-04-17 21:43:35 +0200

    Mise à jour de l'appel api

--- front/src/app/services/contact/contact.service.ts
+++ front/src/app/services/contact/contact.service.ts
@@ -18,7 +18,11 @@ export class ContactService {
       .set('message', data.message);
 
     return this.http.post(this.url, body.toString(), {
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+      headers: {
+        'Content-Type':  'application/json',
+        'X-Site-Domain': 'augustemanou.com',
+        'X-Api-Key':     '3dfe7a62d001aa9142cca9a1c7c184c158b2caacae49f6ca61471b99a44e34c0'
+      }
     });
   }
 }
